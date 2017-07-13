820dcf01b02a218acb3d11542d921e01fc859219
Sergey Pirogov <sergio_89@ukr.net>
2017-07-13 21:28:31 +0300

    kirk added

--- src/test/kotlin/demo/tests/CalculatorTest.kt
+++ src/test/kotlin/demo/tests/CalculatorTest.kt
@@ -1,6 +1,8 @@
 package demo.tests
 
 import com.automation.remarks.kirk.Browser
+import com.automation.remarks.kirk.KElement
+import com.automation.remarks.kirk.Page
 import com.automation.remarks.kirk.conditions.have
 import com.automation.remarks.kirk.core.drive
 import org.openqa.selenium.By
@@ -22,12 +24,46 @@ class CalculatorTest {
             element("h2.ng-binding").should(have.text("4"))
         }
     }
+
+    @Test fun testCanDivide() {
+        System.setProperty("kirk.baseUrl", "http://juliemr.github.io/protractor-demo/")
+        Browser.drive {
+            to(::Calculator) {
+                first.setValue(10)
+                second.setValue(2)
+                select.selectByVisibleText("/")
+                goBtn.click()
+                result.shouldBe(5)
+            }
+        }
+    }
+}
+
+class Calculator(browser: Browser) : Page(browser) {
+
+    override val url: String?
+        get() = "/"
+
+    val first = element("input[ng-model='first']")
+    val second = element("input[ng-model='second']")
+    val goBtn = element("#gobutton")
+    val result = element("h2.ng-binding")
+
+    val select by lazy { browser.select("select[ng-model='operator']") }
 }
 
 fun ngModel(model: String): By {
     return By.xpath("[ng-model='$model']")
 }
 
-fun Browser.select(by:String): Select {
+fun Browser.select(by: String): Select {
     return Select(element(by).webElement)
+}
+
+fun KElement.setValue(value: Any) {
+    this.setValue(value.toString())
+}
+
+fun KElement.shouldBe(value: Any) {
+    this.should(com.automation.remarks.kirk.conditions.have.text(value.toString()))
 }

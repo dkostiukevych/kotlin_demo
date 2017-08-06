fc30d884c8eafe4f61a6e42dc1ff1fb2cccb545e
Sergey Pirogov <sergio_89@ukr.net>
2017-08-06 12:33:01 +0300

    piggy added

--- src/test/kotlin/piggy/PiggyTest.kt
+++ src/test/kotlin/piggy/PiggyTest.kt
@@ -1,6 +1,8 @@
 package piggy
 
+import com.automation.remarks.kirk.Browser
 import com.automation.remarks.kirk.Kirk.Companion.drive
+import com.automation.remarks.kirk.Page
 import com.automation.remarks.kirk.conditions.text
 import org.testng.annotations.Test
 
@@ -13,14 +15,32 @@ class PiggyTest {
     fun testCanAddIncome() {
         drive {
             to("http://my-piggymetrics.rhcloud.com/")
-            element("#frontloginform").setValue("qaswdefrgt").pressEnter()
-            element("#frontpasswordform").setValue("123456").pressEnter()
-            element("#lefttitle").shouldHave(text("QASWDEFRGT METRICS"))
-            element("#plusborder").click()
-            element("#noincomes .majorplusitem").click()
-            element("#add-modal [name='modalvalue']").setValue("1000")
-            element(".modaltitle").setValue("Salary").pressEnter()
-            element(".title9museo300").shouldHave(text("1 000 $ / PER MONTH"))
+            at(::Login).loginAs("qaswdefrgt", "123456")
+            at(::MainPage) {
+                addIncome("Salary", 1000)
+                incomeItem.shouldHave(text("1 000 $ / PER MONTH"))
+            }
         }
     }
+}
+
+class Login(browser: Browser) : Page(browser) {
+
+    fun loginAs(name: String, pass: String) {
+        element("#frontloginform").setValue(name).pressEnter()
+        element("#frontpasswordform").setValue(pass).pressEnter()
+        element("#plusborder").click()
+    }
+}
+
+class MainPage(browser: Browser) : Page(browser) {
+
+    val incomeItem = element(".title9museo300")
+
+    fun addIncome(name: String, value: Int) {
+        element("#noincomes .majorplusitem").click()
+        element("#add-modal [name='modalvalue']").setValue(value.toString())
+        element(".modaltitle").setValue(name).pressEnter()
+    }
+
 }

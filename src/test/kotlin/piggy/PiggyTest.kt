e7e6b6dab094570ca2bb2e9eaf6f198282334c84
Sergey Pirogov <sergio_89@ukr.net>
2017-08-22 22:08:25 +0300

    kirk 0.8.2

--- src/test/kotlin/piggy/PiggyTest.kt
+++ src/test/kotlin/piggy/PiggyTest.kt
@@ -2,12 +2,11 @@ package piggy
 
 import com.automation.remarks.kirk.Browser
 import com.automation.remarks.kirk.Kirk.Companion.drive
+import com.automation.remarks.kirk.Kirk.Companion.open
 import com.automation.remarks.kirk.Page
+import com.automation.remarks.kirk.conditions.clickable
 import com.automation.remarks.kirk.conditions.text
-import org.openqa.selenium.remote.DesiredCapabilities
-import org.openqa.selenium.remote.RemoteWebDriver
 import org.testng.annotations.Test
-import java.net.URI
 
 
 /**
@@ -17,16 +16,16 @@ class PiggyTest {
 
     @Test
     fun testCanAddIncome() {
-        val browser = DesiredCapabilities()
-        browser.browserName = "chrome"
-        browser.version = "59.0"
+//        val browser = DesiredCapabilities()
+//        browser.browserName = "chrome"
+//        browser.version = "59.0"
+//
+//        val driver = RemoteWebDriver(
+//                URI.create("http://35.202.183.239:4444/wd/hub").toURL(),
+//                browser
+//        )
 
-        val driver = RemoteWebDriver(
-                URI.create("http://35.202.183.239:4444/wd/hub").toURL(),
-                browser
-        )
-
-        drive(driver) {
+        drive {
             to("http://my-piggymetrics.rhcloud.com/")
             at(::Login).loginAs("qaswdefrgt", "123456")
             at(::MainPage) {
@@ -35,15 +34,24 @@ class PiggyTest {
             }
         }
     }
+
+    @Test
+    fun testCatDeleteIncome() {
+        open(::Login)
+                .loginAs("qaswdefrgt", "123456")
+                .deleteIncome()
+    }
 }
 
 class Login(browser: Browser) : Page(browser) {
 
-    fun loginAs(name: String, pass: String) {
+    override val url = "http://my-piggymetrics.rhcloud.com/"
+
+    fun loginAs(name: String, pass: String): MainPage {
         element("#frontloginform").setValue(name).pressEnter()
         element("#frontpasswordform").setValue(pass).pressEnter()
         element("#plusborder").click()
-
+        return page(::MainPage)
     }
 }
 
@@ -52,9 +60,17 @@ class MainPage(browser: Browser) : Page(browser) {
     val incomeItem = element(".title9museo300")
 
     fun addIncome(name: String, value: Int) {
-        element("#noincomes .majorplusitem").click()
+        element("#incomesplusitem").waitUntil(clickable).click()
         element("#add-modal [name='modalvalue']").setValue(value.toString())
         element(".modaltitle").setValue(name).pressEnter()
     }
 
+    fun deleteIncome() {
+        element("#incomeslider").click()
+        element(".modal-delete").click()
+    }
+}
+
+fun <T : Page> Page.page(pageClass: (Browser) -> T): T {
+    return at(pageClass)
 }
